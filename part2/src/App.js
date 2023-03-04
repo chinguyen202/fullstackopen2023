b46c5ffc9f42f6f4bdb3a987e6e2dbd3941de686
Chi Nguyen <chi.nguyen@live.com>
2023-03-04 02:39:25 +0200

    [part2-courseinfo]refactor component

---
+++ part2/src/App.js
@@ -0,0 +1,54 @@
+const Header = ({ course }) => <h1>{course}</h1>;
+
+const Total = ({ sum }) => <p>Number of exercises {sum}</p>;
+
+const Part = ({ part }) => (
+  <p>
+    {part.name} {part.exercises}
+  </p>
+);
+
+const Content = ({ parts }) =>
+  parts.map((part, id) => <Part key={id} part={part} />);
+
+const Course = ({ course }) => {
+  let sum = 0;
+  for (let i = 0; i < course.parts.length; i++) {
+    sum += course.parts[i].exercises;
+  }
+  return (
+    <>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total sum={sum} />
+    </>
+  );
+};
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1,
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2,
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3,
+      },
+    ],
+  };
+
+  return <Course course={course} />;
+};
+
+export default App;
