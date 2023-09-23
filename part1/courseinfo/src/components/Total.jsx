1fa990566098f5f3d9dfe1875d3c4ae52467509d
Chi Nguyen <chi.nguyen@live.com>
2023-09-24 00:05:58 +0300

    part1 courseinfo step 4

--- part1/courseinfo/src/components/Total.jsx
+++ part1/courseinfo/src/components/Total.jsx
@@ -3,7 +3,9 @@ const Total = (props) => {
     <>
       <p>
         Number of exercises{' '}
-        {props.exercises1 + props.exercises2 + props.exercises3}
+        {props.parts[0].exercises1 +
+          props.parts[1].exercises2 +
+          props.parts[2].exercises3}
       </p>
     </>
   );
