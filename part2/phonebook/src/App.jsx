a81b03c46cd45f1c239193afba0dde851cb5db9e
Chi Nguyen <chi.nguyen@live.com>
2023-09-24 23:33:33 +0300

    part2 phonebook step 1

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -4,13 +4,24 @@ const App = () => {
   const [persons, setPersons] = useState([{ name: 'Arto Hellas' }]);
   const [newName, setNewName] = useState('');
 
+  const handleInputName = (event) => {
+    event.preventDefault();
+    setNewName(event.target.value);
+  };
+
+  const addPerson = (event) => {
+    event.preventDefault();
+    const newPerson = { name: newName };
+    setPersons(persons.concat(newPerson));
+  };
+
   return (
     <>
       <div>
         <h2>Phonebook</h2>
-        <form>
+        <form onSubmit={addPerson}>
           <div>
-            name: <input />
+            name: <input value={newName} onChange={handleInputName} />
           </div>
 
           <div>
@@ -20,6 +31,9 @@ const App = () => {
         <div>DEBUG: {newName}</div>
         <h2>Numbers</h2>
         ...
+        {persons.map((item, index) => (
+          <p key={index}>{item.name}</p>
+        ))}
       </div>
     </>
   );
